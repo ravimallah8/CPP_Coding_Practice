3224eae63ceaf8feb669a0f6b0d9e22808985119
Ravi Mallah <ravimallah8@gmail.com>
2021-08-11 02:20:46 +0530

    merge 2 sorted array

--- Array/Find duplicate in an array of n + 1 integers.cpp
+++ Array/Find duplicate in an array of n + 1 integers.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 using namespace std;
 
+
 int main()
 {
 	int n;
@@ -8,7 +9,35 @@ int main()
 	int arr[n];
 	for (int i = 0; i <= n; i++)
 		cin >> arr[i];
-	for (int i = 0; i <= n; i++){
+	if (n == 0){
+		cout << "-1";
+	}
+	else{
+		int slow = arr[0];
+		int fast = arr[0];
+		do{
+			slow = arr[slow];
+			fast = arr[arr[fast]];
+		}while (slow != fast);
+
+		fast = arr[0];
+		while (fast != slow){
+			fast = arr[fast];
+			slow = arr[slow];
+		}
+		cout << fast;
+	}
+	return 0;
+}
+/*
+int main()
+{
+	int n;
+	cin >> n;
+	int arr[n];
+	for (int i = 0; i <= n; i++)
+		cin >> arr[i];
+	for (int i = 0; i <=	 n; i++){
 		if (arr[abs(arr[i])] < 0){
 			cout << abs(arr[i]);
 			break;
@@ -18,3 +47,4 @@ int main()
 	}
 	return 0;
 }
+*/
---
+++ Array/Merge 2 sorted arrays without using Extra space.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	int n, m;
+	cin >> n >> m;
+	vector <int> arr1(n), arr2(m);
+	for (int i = 0; i < n; i++)
+		cin >> arr1[i];
+	for (int i = 0; i < m; i++)
+		cin >> arr2[i];
+	int i = 0, j = 0;
+	while (i < n){
+		// if last element of arr1 is less then arr2 first element then break
+		if (arr1[n - 1] <= arr2[0])
+			break;
+		while (j < m){
+			if (arr1[i] > arr2[j]){
+				swap(arr1[i], arr2[j]);
+				i++;
+				break;
+			}
+			j++;
+		}
+		sort(arr2.begin(), arr2.end());
+		
+
+	}
+	for (int i = 0; i < n; i++)
+		cout << arr1[i] << " ";
+	for (int i = 0; i < m; i++)
+		cout << arr2[i] << " ";
+	return 0;
+}
