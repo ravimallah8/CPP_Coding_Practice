623096ebd0aea51e65a015a55a61b1fef3853496
Ravi Mallah <ravimallah8@gmail.com>
2021-08-12 02:18:55 +0530

    Merge Intervals done in O(nlog(n))

--- Array/Merge Intervals.cpp
+++ Array/Merge Intervals.cpp
@@ -3,6 +3,41 @@
 #include <vector>
 using namespace std;
 
+
+int main()
+{
+	int n;
+	cin >> n;
+	vector <vector<int>> intervals(n);
+	for (int i = 0; i < n; i++){
+		intervals[i] = vector<int> (2);
+		cin >> intervals[i][0];
+		cin >> intervals[i][1];
+	}
+	sort(intervals.begin(), intervals.end());
+	vector <vector<int>> merge;
+	vector <int> current;
+	current.push_back(intervals[0][0]);
+	current.push_back(intervals[0][1]);
+	int i = 1;
+	for (i = 1; i < n; i++){
+		if (current[1] >= intervals[i][0]){
+			current[1] = intervals[i][1];
+		}
+		else{
+			merge.push_back(current);
+			current[0] = intervals[i][0];
+			current[1] = intervals[i][1];
+		}
+	}
+	merge.push_back(current);
+	for (int i = 0; i < merge.size(); i++){
+		cout << merge[i][0] << " " << merge[i][1] << " ";
+	}
+	return 0;
+}
+
+/*
 int main()
 {
 	int n;
@@ -39,3 +74,4 @@ int main()
 	cout << "]";
 	return 0;
 }
+*/
